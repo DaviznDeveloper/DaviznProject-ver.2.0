2f478a833c8e96d27da4551c59ce0df9239a4ae3
egoing <sklee690@gmail.com>
2016-06-14 16:57:06 +0900

    [NoteDto.java] - 노트 DTO 추가

---
+++ DaviznProject_Renual/src/main/java/kr/or/davizn/model/dto/NoteDTO.java
@@ -0,0 +1,30 @@
+package kr.or.davizn.model.dto;
+
+public class NoteDTO {
+	
+	private int dataseq;
+	private int datatype;
+	private String filepath;
+	
+	public int getDataseq() {
+		return dataseq;
+	}
+	public void setDataseq(int dataseq) {
+		this.dataseq = dataseq;
+	}
+	public int getDatatype() {
+		return datatype;
+	}
+	public void setDatatype(int datatype) {
+		this.datatype = datatype;
+	}
+	public String getFilepath() {
+		return filepath;
+	}
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
+	
+	
+	
+}
